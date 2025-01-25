8e3e9d6083af79c2a5e11019b9a2c57b884d3e6e
AP <99053162+BrutuallyOP@users.noreply.github.com>
2025-01-25 20:58:29 +0000

    remove unused icon imports

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,3 @@
-
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import Sign from './pages/homepage';
 import Dashboard from './pages/dashboard'
 import React, {useEffect, useState} from 'react';
